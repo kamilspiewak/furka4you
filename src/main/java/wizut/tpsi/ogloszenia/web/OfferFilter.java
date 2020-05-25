6501249df6c72376c52c4d136f69ad3e090d327e
Kamil <kamilspiewak97@gmail.com>
2020-05-25 15:26:53 +0200

    furka_cw3_preparations

---
+++ src/main/java/wizut/tpsi/ogloszenia/web/OfferFilter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wizut.tpsi.ogloszenia.web;
+
+/**
+ *
+ * @author Kamil
+ */
+public class OfferFilter {
+    private Integer manufacturerId;
+    private Integer modelId;
+
+    public Integer getManufacturerId() {
+        return manufacturerId;
+    }
+
+    public void setManufacturerId(Integer manufacturerId) {
+        this.manufacturerId = manufacturerId;
+    }
+
+    public Integer getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(Integer modelId) {
+        this.modelId = modelId;
+    }
+    
+}
